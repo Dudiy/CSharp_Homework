4cc7fae527b228440828618315a9d4336e51cafa
דודי י <dudi.2705@gmail.com>
2017-05-20 20:44:05 +0300

    added IsValidSequence() method to LetterSequence

--- B17 Ex02 DudiYecheskel 200441749 OrMantzur 204311997/B17_Ex02_Program/LetterSequence.cs
+++ B17 Ex02 DudiYecheskel 200441749 OrMantzur 204311997/B17_Ex02_Program/LetterSequence.cs
@@ -9,7 +9,6 @@ namespace B17_Ex02
         private const char k_MaxLetterInSequence = 'H';
         private const byte k_LengthOfSequence = 4;           //this value must be less than or equal to (k_MaxLetterInSequence-'A')
         private string m_Sequence = String.Empty;
-        private int i = 3;
 
         public LetterSequence()
         {
@@ -27,17 +26,18 @@ namespace B17_Ex02
             }
         }
 
-        public byte LengthOfSequence
-        {
-            get { return k_LengthOfSequence; }
-        }
-
         public LetterSequence(string i_Sequence)
         {
-            //valid =   sequence of exactly "k_LengthOfSequence" letters, upper or lower case letters between 'A' and "k_MaxLetterInSequence"
-            //          same letter can appear more than once
-            bool isValidSequence = false;
+            m_Sequence = i_Sequence.Replace(" ", "");
 
+            if (!IsValidSequence(i_Sequence,out string dummy))
+            {
+                //Throw execption;
+            }
+
+            m_Sequence = i_Sequence.ToUpper();
+
+            /*
             while (i_Sequence.Length != k_LengthOfSequence)
             {
                 //TODO throw execption?
@@ -62,8 +62,42 @@ namespace B17_Ex02
                     }
                 }
             }
+            */
+        }
 
-            m_Sequence = i_Sequence;
+        public static byte LengthOfSequence
+        {
+            get { return k_LengthOfSequence; }
+        }
+
+        public static bool IsValidSequence(string i_Sequence, out string o_validationString)
+        {
+            //valid =   sequence of exactly "k_LengthOfSequence" letters, upper or lower case letters between 'A' and "k_MaxLetterInSequence"
+            //          same letter can appear more than once
+            bool isValid = true;
+            string validationResultString = "The input is valid";
+
+            i_Sequence = i_Sequence.Replace(" ", "");           //remove all spaces in the input string
+            if (i_Sequence.Length != k_LengthOfSequence)        //check length of sequence
+            {
+                isValid = false;
+                validationResultString = "length of sequence is invalid";
+            }
+            else
+            {                                                   
+                foreach (char ch in i_Sequence)                 //check letters of sequence
+                {
+                    if (ch < 'a' || Char.ToLower(k_MaxLetterInSequence) > ch &&
+                        ch < 'A' && k_MaxLetterInSequence > ch)
+                    {
+                        validationResultString = String.Format("Invalid sequence, please use upper/lower case letters between 'A' and '{0}':", k_MaxLetterInSequence);
+                        break;
+                    }
+                }
+            }
+
+            o_validationString = validationResultString;
+            return isValid;
         }
 
         public string Compare(LetterSequence i_CompareTo)
@@ -98,11 +132,6 @@ namespace B17_Ex02
             result.Append('X', correctLetterWrongPositionCounter);
 
             return result.ToString();
-        }
-
-        public void Print()
-        {
-            Console.Write(m_Sequence);
         }
     }
 }
