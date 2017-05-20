5b7685569e0405555ea5375e7ba9241f4580679b
דודי י <dudi.2705@gmail.com>
2017-05-20 14:46:21 +0300

    test2

--- B17 Ex02 DudiYecheskel 200441749 OrMantzur 204311997/B17_Ex02_Program/Program.cs
+++ B17 Ex02 DudiYecheskel 200441749 OrMantzur 204311997/B17_Ex02_Program/Program.cs
@@ -10,7 +10,7 @@ namespace B17_Ex02
         {
             GameManager game = new GameManager();
 
-            Console.WriteLine();
+            Console.WriteLine("World");
             game.Start();
 
         }
